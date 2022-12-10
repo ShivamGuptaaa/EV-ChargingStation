2d11ea9b50bd6d8a97f7019e10cb254dc730efb8
Shivam Gupta <iamshivamgp@gmail.com>
2022-12-10 17:44:38 +0530

    Added uni test in bat file

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
+::Start Uni Tests
+dotnet watch test --project tests\EV.CharginStation.Api.UnitTests\EV.CharginStation.Api.UnitTests.csproj
+
+::Start Project
 dotnet run --project src\WebAPI\EV.ChargingStation.Api\EV.ChargingStation.csproj
